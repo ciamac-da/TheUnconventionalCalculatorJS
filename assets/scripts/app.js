3b5a82409278d70b395f265a0c4f548b6b9719b9
the-last-martian <the-last-martian@martian.localdomain>
2020-08-05 10:58:33 +0200

    Verified by Cia v1.0.2  now i can also get previous result

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,8 +2,9 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
+  const calcDescription = `${currentResult} + ${userInput.value}`
   currentResult = currentResult + parseFloat(userInput.value);
-  outputResult(currentResult, '');
+  outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
