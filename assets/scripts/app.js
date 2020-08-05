f5cf3a1132b71f9451d6b9fcc5bf5f2ea508f1e6
the-last-martian <the-last-martian@martian.localdomain>
2020-08-05 11:11:01 +0200

    Verified by Cia v1.0.2  add other functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,4 +12,26 @@ function add() {
   outputResult(currentResult, calcDescription);
 }
 
+function subtract() {
+  const enteredNumber = getUserNumberInput()
+  const calcDescription = `${currentResult} - ${enteredNumber}`
+  currentResult = currentResult - enteredNumber ;
+  outputResult(currentResult, calcDescription);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput()
+  const calcDescription = `${currentResult} * ${enteredNumber}`
+  currentResult = currentResult * enteredNumber ;
+  outputResult(currentResult, calcDescription);
+}
+
+function add() {
+  const enteredNumber = getUserNumberInput()
+  const calcDescription = `${currentResult} / ${enteredNumber}`
+  currentResult = currentResult / enteredNumber ;
+  outputResult(currentResult, calcDescription);
+}
+
+
 addBtn.addEventListener('click', add);
